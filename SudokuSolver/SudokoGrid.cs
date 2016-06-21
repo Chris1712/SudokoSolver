9221278303c746a7e32de5136bbcdefc20683346
Chris Turner <christurner5@gmail.com>
2016-06-21 21:57:32 +0100

    Added part of the logic to check if a grid is valid

--- SudokuSolver/SudokoGrid.cs
+++ SudokuSolver/SudokoGrid.cs
@@ -36,16 +36,58 @@ namespace SudokuSolver
         public bool IsComplete {
             get
             {
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (sudokuArray[i,j] == ' ')
+                        {
+                            return false;
+                        }
+                    }
+                }
                 return true;
             }
 
 
         }
 
-        public bool IsSolved
+        public bool IsValid
         {
             get
             {
+                // Check rows have no repeated values:
+                for (int i = 0; i < 9; i++)
+                {
+                    var rowArray = new List<char>();
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (sudokuArray[i, j] == ' ')
+                            continue;
+                        if (rowArray.Contains(sudokuArray[i,j]))
+                            return false;
+                        else
+                            rowArray.Add(sudokuArray[i, j]);
+                    }
+                }
+                // Check cols have no repeated values:
+                for (int j = 0; j < 9; j++)
+                {
+                    var colArray = new List<char>();
+                    for (int i = 0; i < 9; i++)
+                    {
+                        if (sudokuArray[i, j] == ' ')
+                            continue;
+                        if (colArray.Contains(sudokuArray[i, j]))
+                            return false;
+                        else
+                            colArray.Add(sudokuArray[i, j]);
+                    }
+                }
+
+                // Check squares have no repeated values:
+                // TODO
+
                 return true;
             }
         }
