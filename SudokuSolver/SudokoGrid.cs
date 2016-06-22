a24ada8e775d4e17e14cb4d37fc97b5b829a5876
Chris Turner <christurner5@gmail.com>
2016-06-22 07:14:42 +0100

    Added more validity checking code

--- SudokuSolver/SudokoGrid.cs
+++ SudokuSolver/SudokoGrid.cs
@@ -56,37 +56,104 @@ namespace SudokuSolver
         {
             get
             {
+                // todo, possible efficiency; while computing this keep track of which row/square/col has greatest # of entries, use as place to enter next
+
                 // Check rows have no repeated values:
                 for (int i = 0; i < 9; i++)
                 {
-                    var rowArray = new List<char>();
+                    var rowList = new List<char>();
                     for (int j = 0; j < 9; j++)
                     {
                         if (sudokuArray[i, j] == ' ')
                             continue;
-                        if (rowArray.Contains(sudokuArray[i,j]))
+                        if (rowList.Contains(sudokuArray[i,j]))
                             return false;
                         else
-                            rowArray.Add(sudokuArray[i, j]);
+                            rowList.Add(sudokuArray[i, j]);
                     }
                 }
                 // Check cols have no repeated values:
                 for (int j = 0; j < 9; j++)
                 {
-                    var colArray = new List<char>();
+                    var colList = new List<char>();
                     for (int i = 0; i < 9; i++)
                     {
                         if (sudokuArray[i, j] == ' ')
                             continue;
-                        if (colArray.Contains(sudokuArray[i, j]))
+                        if (colList.Contains(sudokuArray[i, j]))
                             return false;
                         else
-                            colArray.Add(sudokuArray[i, j]);
+                            colList.Add(sudokuArray[i, j]);
                     }
                 }
 
-                // Check squares have no repeated values:
-                // TODO
+                // Check subsquares have no repeated values:
+                var squareEntries = new List<char>();
+                // 0,0 -> 2,2
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (sudokuArray[i, j] == ' ')
+                            continue;
+                        if (squareEntries.Contains(sudokuArray[i, j]))
+                            return false;
+                        else
+                            squareEntries.Add(sudokuArray[i, j]);
+                    }
+                }
+                // 0,6 -> 2,8
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 6; j < 9; j++)
+                    {
+                        if (sudokuArray[i, j] == ' ')
+                            continue;
+                        if (squareEntries.Contains(sudokuArray[i, j]))
+                            return false;
+                        else
+                            squareEntries.Add(sudokuArray[i, j]);
+                    }
+                }
+                // 3,3 -> 5,5
+                for (int i = 3; i < 6; i++)
+                {
+                    for (int j = 3; j < 6; j++)
+                    {
+                        if (sudokuArray[i, j] == ' ')
+                            continue;
+                        if (squareEntries.Contains(sudokuArray[i, j]))
+                            return false;
+                        else
+                            squareEntries.Add(sudokuArray[i, j]);
+                    }
+                }
+                // 6,0 -> 8,2
+                for (int i = 6; i < 9; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (sudokuArray[i, j] == ' ')
+                            continue;
+                        if (squareEntries.Contains(sudokuArray[i, j]))
+                            return false;
+                        else
+                            squareEntries.Add(sudokuArray[i, j]);
+                    }
+                }
+                // 6,6 -> 8,8
+                for (int i = 6; i < 9; i++)
+                {
+                    for (int j = 6; j < 9; j++)
+                    {
+                        if (sudokuArray[i, j] == ' ')
+                            continue;
+                        if (squareEntries.Contains(sudokuArray[i, j]))
+                            return false;
+                        else
+                            squareEntries.Add(sudokuArray[i, j]);
+                    }
+                }
 
                 return true;
             }
