837c66e165c98c82e4b7d110e62647bbda29347d
Chris <christurner5@gmail.com>
2016-06-22 11:24:18 +0100

    Laid out thoughts for recursive solving algorithm

--- SudokuSolver/Form1.cs
+++ SudokuSolver/Form1.cs
@@ -59,12 +59,27 @@ namespace SudokuSolver
             {
                 MessageBox.Show("Invalid sudoku");
             }
-
-            // todo : suggest next place to go in a messagebox MessageBox.Show()
+            else
+            {
+                //MessageBox.Show("Go in these coords:" + SudokoGridToSolve.NextPositionToPopulate().Item1 + "," + SudokoGridToSolve.NextPositionToPopulate().Item2);
+            }
             
         }
 
+        private SudokoGrid RecursiveSolve(SudokoGrid gridToSolve)
+        {
+            // todo: put this as a static method in the sudokugrid class? Not really part of the form logic
 
+            // check if grid is valid, if not return null
+            // check if grid is complete, if it is, return that grid
+
+            // else we have a grid that's incomplete and might have solutions, so:
+            // get the next place to play from the nextpositiontopopulate method, and call self with that for all suggested values. If any of those return a grid, stop and return it.
+            
+            
+            // otherwise the grid we've been given has no solutions, so
+            return null;
+        }
 
         private SudokoGrid GetSudokuGridFromTextBoxes()
         {
@@ -88,7 +103,6 @@ namespace SudokuSolver
             var mySudokuGrid = new SudokoGrid(formCharArray);
             return mySudokuGrid;
         }
-
         private void ResetTextBoxes()
         {
             for (int i = 0; i < 9; i++)
